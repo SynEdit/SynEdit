SynEdit/SynEdit

commit 31f2bcdbea6f04f5b854bd83c2cb0ea39f6bebc4

Use application level 'stay on top' instead of windows global

Source/SynCompletionProposal.pas
@@ -2458,7 +2458,20 @@ begin
     exit;
   end;
 
+{$IFDEF SYN_DELPHI_XE_UP}
+  if Assigned(Application.MainForm) then
+  begin
+    Form.PopupMode := pmExplicit;
+    Form.PopupParent := Application.MainForm;
+  end
+  else
+  begin
+    Form.PopupMode := pmNone;
+    Form.FormStyle := fsStayOnTop;
+  end;
+{$ELSE}
   Form.FormStyle := fsStayOnTop;
+{$ENDIF}
 
   if Assigned(Form.CurrentEditor) then
   begin

commit cbf6099ec2b56750e6233a171df356a40d76b98d

Fix for #43: Wrong Z-order when SynEdit is used in a modal dialog (was broken in 31f2bcdbea6f04f5b854bd83c2cb0ea39f6bebc4 )

Source/SynCompletionProposal.pas
@@ -2451,6 +2451,9 @@ procedure TSynBaseCompletionProposal.ExecuteEx(s: UnicodeString; x, y: integer; 
 
 var
   TmpOffset: Integer;
+  {$IFDEF SYN_DELPHI_XE_UP}
+  ParentForm: TCustomForm;
+  {$ENDIF}
 begin
   DisplayType := Kind;
 
@@ -2466,10 +2469,11 @@ begin
   end;
 
 {$IFDEF SYN_DELPHI_XE_UP}
-  if Assigned(Application.MainForm) then
+  ParentForm := GetParentForm(Form.CurrentEditor);
+  if Assigned(ParentForm) then
   begin
     Form.PopupMode := pmExplicit;
-    Form.PopupParent := Application.MainForm;
+    Form.PopupParent := ParentForm;
   end
   else
   begin